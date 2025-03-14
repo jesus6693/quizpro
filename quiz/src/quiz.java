522fa6f804183ef37c043bc4055f26957ee45b0f
jesus <jesustorres200503@gmail.com>
2025-03-14 07:40:51 -0500

    se agrego ejercicio quiz

--- quiz/src/quiz.java
+++ quiz/src/quiz.java
@@ -1,10 +1,29 @@
 import java.util.Scanner;
 
 public class quiz {
-    public static void main(String[] args) {
-      Scanner imput =new Scanner(System.in);
-      System.out.println("ingrese el numero entero limite");
-      int lim=imput.nextInt();
 
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("ingrese el numero entero limite");
+    int lim = input.nextInt();
+    if (lim <= 0) {
+      System.out.println("numero no valido");
+      input.close();
+      return;
     }
+    for (int k = 1; k <= lim; k++) {
+      for (int j = 1; j <= k; j++) {
+        if (j % 2 == 1) {
+          System.out.print(k);
+        } else {
+          System.out.print(k * -1);
+        }
+        if (j < k) {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+
+  }
 }
